2cc86371f15124a660ccaa8843c7be3e7c66d895
adleles <leles.andreia@gmail.com>
2024-11-07 15:15:27 -0300

    Primeiro commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Usar uma imagem leve do JDK 17
+FROM eclipse-temurin:17-jre
+
+# Definir o diretório de trabalho dentro do container
+WORKDIR /ac2_ca
+
+# Copiar o arquivo .jar gerado para o diretório do container
+COPY target/ac2_ca-0.0.1-SNAPSHOT.jar /ac2_ca/ac2_ca-0.0.1-SNAPSHOT.jar
+EXPOSE 8484
+
+# Comando para executar a aplicação com Java 17
+CMD ["java", "-XX:+UseContainerSupport", "-Xmx512m", "-Dserver.port=8585", "-jar", "ac2_ca-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/ac2_project/example/ac2_ca/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package ac2_project.example.ac2_ca.dto;
+
+public class UserDTO {
+    private String name;
+    private String email;
+
+    // Getters e Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+	   
+	    
+	  
